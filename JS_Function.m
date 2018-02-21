ac697c8b6c004b295c78a116be07fb947bdb273a
Ali Heydari <aliheydari@zeiworks.com>
2018-02-20 20:38:42 -0800

    Updated parts for question 1
    added comments and some code to the part i

--- JS_Function.m
+++ JS_Function.m
@@ -1,41 +1,57 @@
+% Ali Heydari 
 % Julia Set 
 % Part i
 
-%phi = inline('z^2 - 1.25')
-phi = inline('z^2')
+
+
+% function definition
+phi = inline('z^2');
+%c = 0;
+% fixed points: 
 fx1 = (1+ sqrt(6))/2 ;
-%fx1 =  -1;
-%fx2 =  1 ;
 fx2 = (1- sqrt(6))/2 ;
 
+
+% points inside are colored black, outside are pink 
 colormap([0 0 0 ; 1 0 1]);
 
+% call the function Julia that computes the filled Julia set
 zk = Julia(phi,fx1,fx2);
 
+% Display the last value of zk
 disp(zk);
 
+% function's input: function phi, two fixed points - fx1 & fx2
 
 function [zk] = Julia(phi,fx1,fx2)
-M = 2 * ones (141,361);
 
-    for j = 1:141
-        y = -.7 + (j-1) * 0.01;
+% an array M of cored points initialized to 2
+M = 2 * ones (100,161);
+
+    for j = 1:141     
         
-        for i = 1:361
+          % imaginary parts btw -.7 and .7
+        y = -.7 + (j-1) * 0.01;
+          % real parts btw -1.8 & 8
+        for i = 1:361         
         x = -1.8 + (i-1) * 0.01;
-        z = x + 1i * y;
-        % return
+        z = x + 1i * y; 
         zk = z;
-        iflag1 = 0;
-        iflag2 = 0;
-        count = 0;
+        % iflag1 and iflag2 count the number of 
+        iflag1 = 0; 
+        % iterations when the root is within 1.e-6 
+        iflag2 = 0; 
+        % total number of iterations
+        count = 0;  
         
         while count < 100 & abs(zk) < 2 & iflag1 < 5 & iflag2 < 5
             
             count = count + 1;
-            zk = phi(zk);
             
-            error1 = abs(zk - fx1);
+            % fixed point iteration
+            zk = phi(zk);       
+            % convergence to fx1
+            error1 = abs(zk - fx1); 
             
                 if error1 < 1.e-6
                     iflag1 = iflag1 + 1;
@@ -43,7 +59,9 @@ M = 2 * ones (141,361);
                     iflag1 = 0;
                 end
                 
-                error2 = abs(zk - fx2);
+            % convergence to fx2
+            error2 = abs(zk - fx2);
+            
                 if error2 < 1.e-6
                     iflag2 = iflag2 + 1;
                     
@@ -53,17 +71,17 @@ M = 2 * ones (141,361);
         end
         
             if iflag1 >= 5 || iflag2 >= 5 || count >= 100
-            
-                M(j,i) = 1;
+                % if orbis bdd 
+                M(j,i) = 1; 
+                          
             end
         
         end
         
     end
     
+    % plot 
     image([-3 3], [-3 3], M);
-    
-    axis xy;
+    % invert axis
+    axis xy; 
 end
-                    
-
