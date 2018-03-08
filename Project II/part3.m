dc3066ef421fca086f4550b9934aa75813ae763a
alina_g <agataullina@ucdavis.edu>
2018-03-07 20:13:54 -0800

    Updated part 3

--- Project II/part3.m
+++ Project II/part3.m
@@ -8,12 +8,14 @@ function OUT = neuron(n, O, w)
 % O = input vectors O_1, O_2, ..., O_n
 % w = input weights w_1, w_2, ..., w_n
 
-for i = 1:n
-    NET(i) = O(i)*w(i); % The givan value NET = sum(O_i * w_i)
-end
-NET = sum(NET);
+net = eye(n);
 
-F_NET = 1/(1+e^(-NET)); % The given sigmoidal function F(NET)
+for i = 1:n
+    net(:,i) = O(:,i)*w(:,i); % The given value NET = sum(O_i * w_i)
+end
+NET = sum(net);
+
+F_NET = 1/(1+exp(-NET)); % The given sigmoidal function F(NET)
 OUT = F_NET;            % Output 
 
 end
